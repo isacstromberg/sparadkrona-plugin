e3f2be955990602a5605d359aa747ed0d611c7fc
Isac Strömberg <isacstromberg@hotmail.com>
2023-04-27 20:03:31 +0200

    ChatGPT API integrated database update functioning

--- request.php
+++ request.php
@@ -2,28 +2,52 @@
 require __DIR__ . '/vendor/autoload.php';
 use Orhanerday\OpenAi\OpenAi;
 
+/*
+Plugin Name: Sparadkrona-plugin
+Description: Allows "Spartips" to be published automaticly on WordPress
+Version: 1.0
+Author: Isac Strömberg
+*/
+
 global $wpdb;
 
 // Function to create a new post
-function create_new_post() {
+function generate_wordpress_article() {
     global $wpdb;
     $open_ai = new OpenAi('');
     
-
     
       // Fetch data from the database table
-      $result = $wpdb->get_results("SELECT * FROM TestDataSparTips WHERE Id = 1");
+      $result = $wpdb->get_results("SELECT * FROM TestDataSparTips WHERE isPresented = 0 ORDER BY ID ASC LIMIT 1");
+      //$result = $wpdb->get_results("SELECT * FROM TestDataSparTips WHERE Id = 5");
       if (!$result) {
           return false;
       }
- 
+    
       $post_title = $result[0]->Title;
       $post_content = $result[0]->Article;
-      $post_id = $result[0]->ID;
+      $post_id = $result[0]->Id;
 
-      //$prompt = $_GET['prompt'];
-      $prompt = "Ge mig ett tips om hur jag kan spara pengar på odla egen mat, du får inte skriva mer än 256 tecken";
-      
+      $test = "kulattkriga";
+      $idConvertedToInt = intval($post_id);
+     $updateDb = $wpdb->update(
+        'TestDataSparTips',
+        array(
+            'Article' => $test,
+            'IsPresented' => 1,
+        ),
+        array(
+            'ID' => $idConvertedToInt,
+        ),
+        array(
+            '%s',// Article is a string
+            '%d', // IsPresented is an int
+            '%d'  // ID is an integer
+        )
+    );
+     
+    
+     /* $prompt = "Ge mig ett spartips kopplat till denna titel {$post_title} som hjälper mig att spara pengar, du får inte skriva mer än 256 tecken";
       $complete = $open_ai->completion([
         'model' => 'text-davinci-003',
         'prompt' => $prompt,
@@ -36,26 +60,10 @@ function create_new_post() {
     ]);
 
     $gptarr = json_decode($complete, true);
-    $generatedArticle = $gptarr['choices'][0]['text'];
-    echo $generatedArticle;
-    //var_dump($complete);
-      
+    $generatedArticle = $gptarr['choices'][0]['text'];*/
+
+    $generatedArticle = "Skönt att inte slösa api";
      
-
-     // Uppdatera posttitel och innehåll i databasen
-     $wpdb->update(
-         'TestDataSparTips',
-         array(
-             'Gpttitle' => $post_title,
-             'Gptarticle' => $generatedArticle,
-         ),
-         array( 'ID' => $post_id ),
-         array( '%s', '%s' ),
-         array( '%d' )
-     );
-
-
-    }
 
   // Kontrollera om ett inlägg med samma titel redan finns
   $existing_post = get_page_by_title($post_title, OBJECT, 'post');
@@ -66,7 +74,7 @@ function create_new_post() {
         // Create the new post
         $post_id = wp_insert_post(array(
             'post_title' => $post_title,
-            'post_content' => $post_content,
+            'post_content' => $generatedArticle,
             'post_status' => 'publish',
             'post_type' => 'post'
         ));
@@ -75,46 +83,34 @@ function create_new_post() {
         if ($post_id == 0) {
             return false;
         }
-    
-// Avschemalägg CRON-händelsen
-wp_clear_scheduled_hook('publish_post_once_event');
-
-    return true;
 }
-
-function schedule_cron_event() {
-  if ( ! wp_next_scheduled( 'publish_post_once_event' ) ) {
-      wp_schedule_single_event( time(), 'publish_post_once_event' );
-  }
-}
-
-// Hook the CRON event to our function
-add_action('publish_post_once_event', 'create_new_post');
-
-// Schedule the CRON event
-schedule_cron_event();
-
-/*$open_ai = new OpenAi('');
-//$prompt = $_GET['prompt'];
-$prompt = "Ge mig ett tips om hur jag kan spara pengar på odla egen mat, du får inte skriva mer än 256 tecken";
-
-$complete = $open_ai->completion([
-    'model' => 'text-davinci-003',
-    'prompt' => $prompt,
-    'temperature' => 0.7,
-    'max_tokens' => 256,
-    'top_p' => 1,
-    'frequency_penalty' => 0,
-    'presence_penalty' => 0
     
-]);
+/*function schedule_cron_event() {
+  /*  wp_clear_scheduled_hook('publish_test');
+    if ( ! wp_next_scheduled( 'publish_test' ) ) {
+        wp_schedule_single_event( time(), 'publish_test' );
+    }*/
 
-$testarr = json_decode($complete, true);
-$test2 = $testarr['choices'][0]['text'];
-echo $test2;
-//var_dump($complete);*/
+ // Get the current time
+ /*$current_time = current_time('timestamp');
+    
+ // Set the desired time of day for the event
+ $event_time = strtotime('7:17pm', $current_time);
+ 
+ // Schedule the event to occur at the desired time every day
+ wp_schedule_event($event_time, 'daily', 'publish_test');
+
+
+  }*/
+  
 
 
 
+
+  // Hook the CRON event to our function
+  add_action('publish_test', 'generate_wordpress_article');
+  
+  // Schedule the CRON event
+  //schedule_cron_event();
 
 ?>
