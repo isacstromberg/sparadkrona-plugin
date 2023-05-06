3da758785c701e98328e1164c64f66487f15cbc7
Isac Strömberg <isacstromberg@hotmail.com>
2023-05-06 13:12:29 +0200

    Automatic picture fixed

--- request.php
+++ request.php
@@ -20,7 +20,7 @@ function generate_wordpress_article() {
     
     $client = new GuzzleHttp\Client();
 
-$response = $client->request('GET', "https://api.unsplash.com/search/photos?query=göteborg&client_id={$unsplashApi}");
+$response = $client->request('GET', "https://api.unsplash.com/search/photos?query=iceland&client_id={$unsplashApi}");
   $data = json_decode($response->getBody(), true);
 $fetchPicture = $data['results'][0]['urls']['thumb'];
 	
@@ -56,7 +56,7 @@ $image_url = $fetchPicture . $jpg;
     );
      
     
-      $prompt = "Ge mig ett spartips kopplat till denna titel {$post_title} som hjälper mig att spara pengar, du får inte skriva mer än 256 tecken";
+   /*   $prompt = "Ge mig ett spartips kopplat till denna titel {$post_title} som hjälper mig att spara pengar, du får inte skriva mer än 256 tecken";
       $complete = $open_ai->completion([
         'model' => 'text-davinci-003',
         'prompt' => $prompt,
@@ -70,9 +70,9 @@ $image_url = $fetchPicture . $jpg;
 
 
     $gptarr = json_decode($complete, true);
-    $generatedArticle = $gptarr['choices'][0]['text']; 
+    $generatedArticle = $gptarr['choices'][0]['text']; */
 
-   // $generatedArticle = "Skönt att inte slösa api";
+    $generatedArticle = "Skönt att inte slösa api";
     
 
   // Kontrollera om ett inlägg med samma titel redan finns
@@ -80,52 +80,101 @@ $image_url = $fetchPicture . $jpg;
   if ($existing_post) {
       return false;
   }
-
-
-//Skapa det nya inlägget och inkludera kategorin
-$post_id = wp_insert_post(array(
-    'post_title' => $post_title,
-    'post_content' => $generatedArticle,
-    'post_status' => 'draft',
+$image_html = '<img src="' . $image_url . '">';
+$postarr = array(
+    'post_title' => 'Snart är det helg =)',
+    'post_content' => $image_html . 'Här är lite text och en bild: ',
+	'post_status' => 'draft',
     'post_type' => 'post',
-
-));
-
-// Tilldela en bild till inlägget
-
-$upload_dir = wp_upload_dir(); // Hämta uppladdningsmappen
-$image_data = file_get_contents($image_url); // Hämta bildens data
-$filename = basename($image_url); // Hämta filnamnet
-$unique_file_name = wp_unique_filename( $upload_dir['path'], $filename ); // Generera ett unikt filnamn
-$file_path = $upload_dir['path'] . '/' . $unique_file_name; // Generera sökvägen till den uppladdade filen
-file_put_contents( $file_path, $image_data ); // Ladda upp bilden till servern
-$wp_filetype = wp_check_filetype( $filename, null ); // Hämta filtypen för bilden
-$attachment = array(
-'post_mime_type' => $wp_filetype['type'],
-'post_title' => $filename,
-'post_content' => '',
-'post_status' => 'inherit'
 );
-$attachment_id = wp_insert_attachment( $attachment, $file_path, $post_id ); // Lägg till bild som bilaga till inlägget
-$attachment_data = wp_generate_attachment_metadata( $attachment_id, $file_path ); // Generera metadata för bilagan
-wp_update_attachment_metadata( $attachment_id, $attachment_data ); // Uppdatera metadata för bilagan
-set_post_thumbnail( $post_id, $attachment_id ); // Tilldela bilden som utvald bild för inlägget
 
+// Spara posten och bifoga bilden
+$post_id = wp_insert_post($postarr);
+// Hämta filnamnet från URL:en
+$file_name = basename($image_url);
+// Hämta filinnehållet från URL:en
+$image_content = file_get_contents($image_url);
+// Hämta WordPress uppladdningskatalogen.
+$wp_upload_dir = wp_upload_dir();
+// Sätt uppladdningsmappens sökväg.
+$upload_path = $wp_upload_dir['path'];
+// Sätt uppladdningsmappens URL.
+$upload_url = $wp_upload_dir['url'];
+// Skapa filvägen
+$file_path = $upload_path . '/' . $file_name;
+
+// Skapa en fil i uppladdningsmappen
+file_put_contents($file_path, $image_content);
+
+// Hämta WordPress uppladdningsfunktionen
+$wp_upload_file = wp_upload_bits($file_name, null, $image_content, date("Y-m", strtotime("now")));
+
+// Lägg till bilden i media biblioteket
+$wp_filetype = wp_check_filetype($file_name, null );
+$attachment = array(
+    'post_mime_type' => $wp_filetype['type'],
+    'post_title' => sanitize_file_name($file_name),
+    'post_content' => '',
+    'post_status' => 'inherit'
+);
+$attachment_id = wp_insert_attachment( $attachment, $wp_upload_file['file'] );	
+$updateDb2 = $wpdb->update(
+        'TestDataSparTips',
+        array(
+            'Picture' => $attachment_id,
+			'Post' => $post_id,
+        ),
+        array(
+            'ID' => $idConvertedToInt,
+        ),
+        array(
+        
+            '%d', // Picture is an int
+			'%d', // Post is an int
+            '%d'  // ID is an integer
+        )
+    );		
+	
+$attachment_data = wp_generate_attachment_metadata( $attachment_id, $wp_upload_file['file'] );
+wp_update_attachment_metadata($attachment_id, $attachment_data);
+	
+
+	// Skapa en array för posten
+
+	
+	
+	
 
 
 // Set the post terms (category)
-wp_set_post_terms($post_id, array('spartips-test'), 'category');
+//wp_set_post_terms($wp_post_id, array('spartips-test'), 'category');
 
         // Check if the post was created successfully
         if ($post_id == 0) {
             return false;
         }
+	
+
 }
-    
+function update_wordpress_article() {
+global $wpdb; 
+$fetchIds = $wpdb->get_results("SELECT * FROM TestDataSparTips WHERE isPresented = 1 ORDER BY ID DESC LIMIT 1");	
+// Skapa ett array med de uppdaterade postuppgifterna
+$post_data = array(
+  'ID' => $fetchIds[0]->Post, // Inläggets ID
+  'post_title' => 'Såja', // Uppdaterad titel
+   'meta_input' => array(
+        '_thumbnail_id' => $fetchIds[0]->Picture// här ersätter du 123 med ID för bilden som du vill bifoga
+    ),
+);
+
+// Uppdatera inlägget med hjälp av wp_update_post-funktionen
+wp_update_post( $post_data );	   
+} 
 
   // Hook the CRON event to our function
   add_action('publish_test', 'generate_wordpress_article');
-  
+  add_action('update_test', 'update_wordpress_article');
 
 
 ?>
